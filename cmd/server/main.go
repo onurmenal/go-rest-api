1f3738a6ea9a71e1ad3d1890fb2ff7fe515fcfc5
Onur Menal <onur.menal@yemeksepeti.com>
2022-05-19 15:44:22 +0300

    Database connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/onurmenal/go-rest-api/internal/db"
+)
 
 func Run() error {
 	fmt.Println("Starting up API")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to the database")
+		return err
+	}
+
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+
+	fmt.Println("Successfully connected and pinged database")
+
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/onurmenal/go-rest-api
 
 go 1.16
+
+require (
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/lib/pq v1.10.6 // indirect
+)
